c6937e0a795b55b144e7afd4416f7b8953b96b5f
Simone Basso <bassosimone@gmail.com>
2015-11-14 20:02:21 +0100

    implement context manager

--- main.cpp
+++ main.cpp
@@ -7,18 +7,17 @@
 
 using namespace relight;
 
-int main() {
-    Var<Poller> poller(new Poller);
+static void create_stream(Var<Poller> poller) {
     Var<Stream> stream(new Stream(poller));
-    poller->call_soon([=]() {
-        std::cerr << "loop running\n";
+    poller->with<Stream>(stream, [=]() {
+        std::cerr << "with\n";
         stream->connect_ipv4("127.0.0.1", 8080, [=]() {
             std::cerr << "connected\n";
             stream->on_data([=](Var<Bytes>) {
                 stream->on_flush([=]() {
                     stream->write("flushed\n");
                     stream->on_data([=](Var<Bytes>) {
-                        poller->break_loop();
+                        poller->break_with<Stream>(stream);
                     });
                     stream->on_flush([]() {});
                 });
@@ -26,14 +25,21 @@ int main() {
             });
             stream->on_error([=](int err) {
                 std::cerr << "error: " << err << "\n";
-                poller->break_loop();
+                poller->break_with<Stream>(stream);
             });
         }, [=](int err) {
             std::cerr << "connect err: " << err << "\n";
-            poller->break_loop();
+            poller->break_with<Stream>(stream);
         });
+    }, [=]() {
+        poller->break_loop();
+        stream->close();
+        std::cerr << "break_with\n";
     });
+}
+
+int main() {
+    Var<Poller> poller(new Poller);
+    create_stream(poller);
     poller->loop();
-    std::cerr << "disconnected\n";
-    stream->close();
 }
--- poller.hpp
+++ poller.hpp
@@ -1,10 +1,13 @@
 #ifndef RELIGHT_POLLER_HPP
 #define RELIGHT_POLLER_HPP
 
+#include "var.hpp"
+
 #include <event2/event.h>
 #include <event2/util.h>
 
 #include <functional>
+#include <map>
 #include <new>
 #include <stdexcept>
 
@@ -40,6 +43,23 @@ class Poller {
         }
     }
 
+    template <typename T> void with(Var<T> t, std::function<void()> init,
+                                    std::function<void()> finish) {
+        auto p = t.get();
+        if (running_.find(p) != running_.end()) {
+            throw std::runtime_error("element already there");
+        }
+        running_[p] = [=]() { finish(); };  // keep t safe
+        call_soon([=]() { init(); });
+    }
+
+    template <typename T> void break_with(Var<T> t) {
+        auto p = t.get();
+        std::function<void()> finish_wrapper = running_.at(p);
+        running_.erase(p);
+        call_soon([=]() { finish_wrapper(); });
+    }
+
     void loop() {
         if (event_base_dispatch(evbase_) != 0) {
             throw std::runtime_error("event_base_dispatch");
@@ -60,6 +80,7 @@ class Poller {
 
   private:
     event_base *evbase_ = nullptr;
+    std::map<void *, std::function<void()>> running_;
 };
 
 }
